aff59890dcb68d5d562dadd28b7d2bcc130e6d24
MarsMig <mars.n.m@outlook.com>
2024-09-10 11:59:49 -0400

    second commit, added notes

--- index.js
+++ index.js
@@ -1,20 +1,21 @@
-//String for message will be done using const variable as user = you
+//String for message will be done using const variable as user = message
 //Answer to lock combination 10-40-39
 // first 10 = 5 * 2
 //second 40 = 160 / 4
 //third 39 = 30 + 9
 
-const user = "you";
+const user =
+  "You have received this message because you have been chosen to open an important vault. Here is the secret combination: ";
 const one = "5x2";
 const two = "160/4";
 const three = "30+9";
 let first = 5 * 2;
 let second = 160 / 4;
 let third = 30 + 9;
-alert(
-  `${user} have received this message because ${user} have been chosen to open an important vault; 
-here is the secret combination: ${one} , ${two} , ${three}`
-);
+alert(`${user}  ${one} , ${two} , ${three}`);
+//ran alert as successful
 console.log(first);
 console.log(second);
 console.log(third);
+
+//included answers to equations in script above
