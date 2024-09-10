0e54c396eb0016aec8633b5c730e0a534e233fb7
MarsMig <mars.n.m@outlook.com>
2024-09-10 11:39:15 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+//String for message will be done using const variable as user = you
+//Answer to lock combination 10-40-39
+// first 10 = 5 * 2
+//second 40 = 160 / 4
+//third 39 = 30 + 9
+
+const user = "you";
+const one = "5x2";
+const two = "160/4";
+const three = "30+9";
+let first = 5 * 2;
+let second = 160 / 4;
+let third = 30 + 9;
+alert(
+  `${user} have received this message because ${user} have been chosen to open an important vault; 
+here is the secret combination: ${one} , ${two} , ${three}`
+);
+console.log(first);
+console.log(second);
+console.log(third);
